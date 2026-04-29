aa23797ca5af25921dc55202a5811dfcc82e57cd
Muhammad Rafiansyah <mraansyah34@gmail.com>
2026-04-29 10:20:55 +0700

    feat: upload files and adding dragable file

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,8 @@ import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
 import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
 import Button from "components/ui/Button";
+import Upload from "components/Upload";
+import { useNavigate } from "react-router";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -11,6 +13,16 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleUpLoadComplete = async (base64Image: string) => {
+    const newId = Date.now().toString();
+
+    navigate(`/visualizer/${newId}`);
+
+    return true;
+  }
+
   return (
     <div className="home">
       <Navbar />
@@ -58,9 +70,7 @@ export default function Home() {
               </p>
             </div>
 
-            <p>
-              Upload images
-            </p>
+            <Upload onComplete={handleUpLoadComplete} />
           </div>
         </div>
       </section>
