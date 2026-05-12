2b48882ef417c9d1bbaa3c9429f60567636ccba4
Muhammad Rafiansyah <mraansyah34@gmail.com>
2026-05-12 21:58:28 +0700

    feat: compare slider

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,8 +9,7 @@ import { createProject, getProjects } from "lib/puter.action";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Roomify" },
   ];
 }
 
